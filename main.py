7edb401debc448452000b2b3790e8aca653f95eb
4nth0ny1 <acatullo4@gmail.com>
2023-12-31 23:18:56 -0600

    loaded levels of csv files

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import csv
 import constants
 from character import Character
 from weapon import Weapon
@@ -12,6 +13,10 @@ pygame.display.set_caption("Dungeon Crawler")
 
 # create clock for maintaining frame rate
 clock = pygame.time.Clock()
+
+
+#define game variables
+level = 1
 
 # define player movement variables
 moving_left = False
@@ -80,13 +85,18 @@ def draw_text(text, font, text_col, x, y):
     screen.blit(img, (x, y))
 
 
-world_data = [
-    [7, 7, 7, 7, 7, 7],
-    [7, 0, 1, 2, 3, 7],
-    [7, 3, 4, 5, 5, 7],
-    [7, 6, 6, 6, 0, 7],
-    [7, 0, 0, 0, 0, 7],
-]
+# create empty tile list
+world_data = []
+for row in range(constants.ROWS):
+    r = [-1] * constants.COLS
+    world_data.append(r)
+# load in level data and create world
+with open(f"levels/level{level}_data.csv", newline="") as csvfile:
+    reader = csv.reader(csvfile, delimiter=",")
+    for x, row in enumerate(reader):
+        for y, tile in enumerate(row):
+            world_data[x][y] = int(tile)
+
 
 world = World()
 world.process_data(world_data, tile_list)
